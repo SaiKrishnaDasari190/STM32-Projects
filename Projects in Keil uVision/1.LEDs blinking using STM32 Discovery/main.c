8863c485c031c4e3e1668fa4409e3b396be68025
SAI KRISHNA DASARI <dsaikrishna703@gmail.com>
2023-02-02 12:17:47 +0530

    Update main.c
    modified alignment

--- Projects in Keil uVision/1.LEDs blinking using STM32 Discovery/main.c
+++ Projects in Keil uVision/1.LEDs blinking using STM32 Discovery/main.c
@@ -10,27 +10,27 @@ static void delay(void)
 
 int main(void)
 	
-	{
-			 LED_Initialize ();
-			 Buttons_Initialize ();
-			
+{
+	 LED_Initialize ();
+	 Buttons_Initialize ();
+
 	while(1)
+	{
+		if(Buttons_GetState()==1)
 		{
-				if(Buttons_GetState()==1)
-				{
-					LED_On(0);
-					LED_On(1);
-					LED_On(2);
-					LED_On(3);
-					delay();
-					LED_Off(0);
-					LED_Off(1);
-					LED_Off(2);
-					LED_Off(3);
-					delay();
-				}
-		
+			LED_On(0);
+			LED_On(1);
+			LED_On(2);
+			LED_On(3);
+			delay();
+			LED_Off(0);
+			LED_Off(1);
+			LED_Off(2);
+			LED_Off(3);
+			delay();
 		}
+
+	}
 		
 	return 0;
 	
